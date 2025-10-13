2412438316ca6f63a2ac6e2d7ab2a588e480bf88
sudhi <sunil9636990666@gmail.com>
2025-10-14 04:02:11 +0530

    .env error sorted

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
 import 'package:feedyou/pages/onboard.dart';
 import 'package:feedyou/widget/app_constant.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_stripe/flutter_stripe.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Load the .env file from project root
+  await dotenv.load(fileName: 'assets/.env');
+
+  // Now set Stripe key
   Stripe.publishableKey = publishableKey;
+
   await Firebase.initializeApp();
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
